004c4fbb4c12b92acca0b00013e2f02802a7c3f0
yang <yang@fydeos.io>
2021-01-20 12:32:05 +0800

    fix log output

--- src/fydeos.com/power_daemon/main.go
+++ src/fydeos.com/power_daemon/main.go
@@ -4,6 +4,7 @@ import (
   "context"
   "time"
   "log"
+  "os"
   "github.com/godbus/dbus"
   "fydeos.com/power_daemon/dbusutil"
   "fydeos.com/power_daemon/suspend_manager"
@@ -11,7 +12,10 @@ import (
 
 
 func main() {
+  log.SetOutput(os.Stdout)
+  log.Println("Waiting for power manager init...")
   time.Sleep(1000 * time.Millisecond)
+  log.Println("Trying connect system bus")
   conn, err := dbus.ConnectSystemBus(dbus.WithSignalHandler(dbus.NewSequentialSignalHandler()))
   if err != nil {
     log.Fatalf("Connect system bus error:%w", err)
